77f280830d2d3ecc0562c58c8875af1d51ef65e4
nellirojascessa <nrojascessa40@stuy.edu>
2024-01-16 00:54:37 -0500

    color bug

--- messaging.c
+++ messaging.c
@@ -62,7 +62,7 @@ int recvmessage(int socket){
   struct package * recieved = malloc ( sizeof(struct package));
   int r_check = recv(socket, recieved, sizeof(struct package), 0);
   if (r_check == 0){
-    printf("socket closed.\n");
+    printf(BLU "A socket has closed." RESET);
     return SOCKETCLOSED;
   }
   err2(r_check, "Recving\n");
@@ -94,7 +94,7 @@ int recvmessagestring(int socket, char * tempuser, char * tempbuff){
   struct package * recieved = malloc ( sizeof(struct package));
   int r_check = recv(socket, recieved, sizeof(struct package), 0);
   if (r_check == 0){
-    printf("socket closed.\n");
+    printf(BLU "A socket has closed." RESET);
     return SOCKETCLOSED;
   }
   err2(r_check, "Recving\n");
